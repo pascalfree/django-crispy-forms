9a7a9381ad540a3805b12e8e2bdd4b0c9bb96e30
William Stewart <zoidbergwill@gmail.com>
2015-10-06 13:09:43 +0200

    Fix tests for new django, drop 1.4 and python 2.6

--- crispy_forms/tests/test_form_helper.py
+++ crispy_forms/tests/test_form_helper.py
@@ -456,7 +456,10 @@ def test_multifield_errors():
 
     form.helper.form_show_errors = True
     html = render_crispy_form(form)
-    assert html.count('error') == 3
+    if django.VERSION < (1, 9):
+        assert html.count('error') == 3
+    else:
+        assert html.count('error') == 1
 
     # Reset layout for avoiding side effects
     form.helper.layout = Layout(
